c97f8f607093911202eb5a6964ccd2a1881794f0
pyyupsk <pyyupsk@proton.me>
2025-05-05 21:19:44 +0700

    chore: update dependencies and ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,54 @@ import pluginJs from '@eslint/js'
 import prettier from 'eslint-plugin-prettier'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import perfectionist from 'eslint-plugin-perfectionist'
 
-export default [
-  { rules: { 'no-unused-vars': 'error', 'no-undef': 'error' } },
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { plugins: { prettier }, rules: { 'prettier/prettier': 'error' } },
+export default tseslint.config(
+  {
+    plugins: {
+      perfectionist,
+      prettier
+    },
+    rules: {
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'natural',
+          order: 'asc'
+        }
+      ],
+      'no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_'
+        }
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_'
+        }
+      ],
+      'prettier/prettier': 'warn'
+    }
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }]
+    }
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended
-]
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier
+)
